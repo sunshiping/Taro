64af075721e02ef0472a605fb98e4ecc1839e307
孙世平 <sunshiping1117@163.com>
2019-05-17 09:55:48 +0800

    搜索框吸顶

---
+++ src/utils/throttle.js
@@ -0,0 +1,14 @@
+export default function throttle(fn: Function, threshold: number = 1500) {
+  if (threshold === null) {
+    threshold = 1500
+  }
+  let _lastExecTime: null | number = null;
+  let context = this
+  return function (...args: any[]): void {
+    let _nowTime: number = new Date().getTime();
+    if (_nowTime - Number(_lastExecTime) > threshold || !_lastExecTime) {
+      fn.apply(context, args);
+      _lastExecTime = _nowTime
+    }
+  }
+}
